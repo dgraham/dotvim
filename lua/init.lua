ad2d88993168713e594798e91ed65afe92466822
David Graham <david.malcom.graham@gmail.com>
2023-08-15 14:24:36 -0600

    Update plugins

--- lua/init.lua
+++ lua/init.lua
@@ -115,6 +115,8 @@ require'nvim-treesitter.configs'.setup {
 
       keymaps = {
         -- You can use the capture groups defined in textobjects.scm
+        ["ab"] = "@block.outer",
+        ["ib"] = "@block.inner",
         ["af"] = "@function.outer",
         ["if"] = "@function.inner",
         ["ac"] = "@class.outer",
