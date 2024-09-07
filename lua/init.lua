fb29653da85d2479d83356ca1fbdb9d972029ca0
David Graham <david.malcom.graham@gmail.com>
2024-09-06 18:42:58 -0600

    Update plugins

--- lua/init.lua
+++ lua/init.lua
@@ -4,7 +4,7 @@ let &packpath=&runtimepath
 source ~/.vimrc
 ]])
 
-require'lspconfig'.tsserver.setup{}
+require'lspconfig'.ts_ls.setup{}
 require'lspconfig'.eslint.setup{}
 -- require'lspconfig'.ruby_ls.setup{}
 require'lspconfig'.rubocop.setup{}
